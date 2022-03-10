94c9a8369a20b3face8d4e12bfcee05e9434948c
Gino Labiste <gino.labiste@gmail.com>
2022-03-10 14:25:32 +0800

    Added transaction model and migration.

---
+++ database/migrations/2022_03_10_033829_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('transaction_code');
+            $table->integer('transact_by');
+            $table->foreign('transact_by')->references('id')->on('students')->onDelete('cascade');
+            $table->string('transaction_type');
+            $table->date('date_completed')->nullable();
+            $table->timestamp('date_submitted');
+            $table->longText('remarks');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
